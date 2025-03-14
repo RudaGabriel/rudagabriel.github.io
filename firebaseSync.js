ac4aa877e38a2ed18102bcbe3383a1c65bffc133
Ruda Gabriel <rudag3@gmail.com>
2025-03-14 12:32:20 -0400

    Update firebaseSync.js

--- firebaseSync.js
+++ firebaseSync.js
@@ -42,7 +42,7 @@ function showCascadeAlert(message) {
     if (!document.querySelector(".cascade-clear-btn")) {
         const clearButton = document.createElement("button");
         clearButton.className = "cascade-clear-btn";
-		clearButton.title = "Limpar todas as mensagens de informações exibidas"; 
+		clearButton.title = "Limpar todas as mensagens de informações exibidas a esquerda"; 
         clearButton.textContent = "<- Limpar todos estes alertas";
         clearButton.addEventListener("click", () => {
             document.querySelectorAll(".cascade-alert").forEach((el) => removeAlert(el));
