0c6870dac4ae484c34d7976c5229d7c246f99fcf
Ruda Gabriel <rudag3@gmail.com>
2025-03-20 12:42:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function adicionarProduto() {
 	if (produtoEditadoIndex === -1 && adicionarBtn.textContent !== "Atualizar") {
 		let produtoExistente = produtos.find(p => p.codigoBarras === codigoBarras && p.nome === nome && formatarData(p.vencimento) === formatarData(vencimento));
 		if (produtoExistente) {
-			msg("Sim", "Não", false, `Produto ${nome} já adicionado<br>Com o mesmo código de barras e data de vencimento!<br>Deseja atualizar a quantidade deste produto com a nova quantidade?<br>Quantidade anterior: ${produtoExistente.quantidade} | Nova quantidade: ${quantidade}`, function () {
+			msg("Sim", "Não", false, `Produto ${nome} já adicionado<br>Com o mesmo código de barras e data de vencimento!<br>Deseja atualizar a quantidade deste produto com a nova fornecida?<br>Quantidade anterior: ${produtoExistente.quantidade} | Nova quantidade: ${quantidade}`, function () {
 					Object.assign(produtoExistente, { nome, quantidade, vencimento, codigoBarras });
 					modal.style.display = "none";
 					salvarProdutos();
