e90ada216ea815287abd2cdd23374dcb11df6bce
Ruda Gabriel <rudag3@gmail.com>
2025-03-24 17:39:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -203,7 +203,7 @@ function removerProduto(nome, vencimento, quantidade) {
 			if (nomeProduto === nome && formatarData(vencimentoProduto) === formatarData(vencimento) && qtdProduto === quantidade) {
 				linha.remove();
 				let produtos = JSON.parse(localStorage.getItem("produtos")) || [];
-				produtos = produtos.filter(prod => !(prod.nome === nome && formatarData(prod.vencimento) === formatarData(vencimento) && prod.quantidade === quantidade));
+				produtos = produtos.filter(prod => !(prod.nome === nome && formatarData(prod.vencimento) === formatarData(vencimento) && parseInt(prod.quantidade) === quantidade));
 				localStorage.setItem("produtos", JSON.stringify(produtos));
 				console.log("Produto removido:", produtos);
 				modal.style.display = "none";
